254d9981312d916e4e28e5494b30a1d0960d509a
gsalicruu <146372806+gsalicruu@users.noreply.github.com>
2023-12-10 17:29:33 +0100

    Update musicManager.cs
    nuevo método pauseresumemusic y modificación de stopmusic

--- Sudoku/musicManager.cs
+++ Sudoku/musicManager.cs
@@ -1,21 +1,59 @@
 using IrrKlang;
 
-public class MusicManager {
+public class MusicManager
+{
 
     private ISoundEngine _soundEngine = new ISoundEngine();
+    private ISound _currentSound;
     private string _music_path;
     private bool _loop;
+    private bool _isPaused;
 
-    public MusicManager(string path_to_music, bool loop ) {
+    public MusicManager(string path_to_music, bool loop)
+    {
         _music_path = path_to_music; //initialize MusicManager with file path
         _loop = loop;
     }
 
-    public void PlayMusic(){
-        _soundEngine.Play2D(_music_path, _loop); // the second parameter set to true makes it play in a loop
+    public void PlayMusic()
+    {
+        _currentSound = _soundEngine.Play2D(_music_path, _loop); // the second parameter set to true makes it play in a loop
     }
 
-    public void StopMusic(){
-        _soundEngine.StopAllSounds();
+    public void PauseResumeMusic()
+    {
+        if (_currentSound != null)
+        {
+            if (_isPaused)
+            {
+                _currentSound.Paused = false; // Reanudar la música
+                _isPaused = false;
+            }
+            else
+            {
+                _currentSound.Paused = true; // Pausar la música
+                _isPaused = true;
+            }
+        }
+    }
+
+    public void StopMusic()
+    {
+        if (_currentSound != null)
+        {
+            _currentSound.Paused = false; 
+            _soundEngine.StopAllSounds();
+            _currentSound = null;
+            _isPaused = false;
+        }
+    }
+    public void AdjustVolume(float volume)
+    {
+        if (_currentSound != null)
+        {
+            // Asegúrate de que el volumen esté en el rango [0, 1]
+            volume = System.Math.Max(0f, System.Math.Min(1f, volume));
+            _currentSound.Volume = volume;
+        }
     }
 }
